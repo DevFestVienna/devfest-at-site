a91f92f65b16a50dfd097e6c0263383bbe5ac00b
Helmuth Breitenfellner <helmuth@breitenfellner.at>
2016-11-05 06:10:52 +0100

    Remove non-current speakers
    from app son

--- automation/json/mobile.js
+++ automation/json/mobile.js
@@ -30,6 +30,7 @@ var speakerNum = {};
 var speakersOut = [];
 var aSpeaker;
 for (i=0; i<speakers.length; i++) {
+  if (!speakers[i].current) continue;
   speakerNum[speakers[i].id] = i+1;
   aSpeaker = {
     'bio': deHtml(speakers[i].bio),
